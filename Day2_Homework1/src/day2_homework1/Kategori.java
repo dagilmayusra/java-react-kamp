6350ee13080f8d67b42ad329557b66eac69fb7a2
Yüsra <76865219+devyusss@users.noreply.github.com>
2021-12-04 20:34:23 +0300

    Delete Day2_Homework1 directory

--- Day2_Homework1/src/day2_homework1/Kategori.java
+++
@@ -1,6 +0,0 @@
-package day2_homework1;
-
-public class Kategori {
-	int id;
-	String name;
-}
--- Day2_Homework1/src/day2_homework1/KursManager.java
+++
@@ -1,7 +0,0 @@
-package day2_homework1;
-
-public class KursManager {
-	public void kursEkle(Kurs kurs) {
-		System.out.println("Kursa eklendiniz : " + kurs.name);
-	}
-}
