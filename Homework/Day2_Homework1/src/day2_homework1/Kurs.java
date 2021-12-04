2e54b1028f43cacbb81c637440f9b98e35f8a948
Yüsra <76865219+devyusss@users.noreply.github.com>
2021-12-04 20:31:15 +0300

    Add files via upload

---
+++ Homework/Day2_Homework1/src/day2_homework1/Kurs.java
@@ -0,0 +1,15 @@
+package day2_homework1;
+
+public class Kurs {
+	
+	
+	public Kurs(int id, String name, int ilerleme) {
+		this.id=id;
+		this.name=name;
+		this.ilerleme=ilerleme;
+	}
+	
+	int id;
+	String name;
+	int ilerleme;
+}
