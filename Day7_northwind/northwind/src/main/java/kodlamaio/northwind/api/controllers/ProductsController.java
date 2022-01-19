db835cfdf39e3accd6eee7875f48ca2ad83d6086
Yüsra <76865219+devyusss@users.noreply.github.com>
2022-01-19 22:53:48 +0300

    Add files via upload

---
+++ Day7_northwind/northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,34 @@
+package kodlamaio.northwind.api.controllers;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController		//controller oldugunu soyluyor
+@RequestMapping("/api/products")		//adres uzerinden ıstek gelırse bu karsılar
+public class ProductsController {
+	
+	//birden fazla service kullanıldıgı zamn autowired kullanılması gerek
+	private final ProductService productService;
+
+    @Autowired
+    public ProductsController(ProductService productService){
+        this.productService = productService;
+    }
+
+	@GetMapping("/getall")	//istekte bulunduğu yeri çalıştırır
+	 public DataResult<List<Product>> getAll(){
+        return productService.getAll();
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody Product product){
+        return productService.add(product);
+    }
+	
+}
---
+++ Day7_northwind/northwind/src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,20 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result{//T data ile döndüreceği veri verildi
+	
+	private T data;
+	public DataResult(T data, boolean success, String message) {
+		super(success, message); //super base classın constructoını calsıtırır yanı Result sınıfının
+		this.data= data;
+	}
+	
+	public DataResult(T data, boolean success) {
+		super(success); 
+		this.data= data;
+	}
+	
+	public T getData() {
+		return this.data;
+	}
+
+}
