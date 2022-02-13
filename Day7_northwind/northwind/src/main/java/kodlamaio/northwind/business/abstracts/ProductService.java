ede193bd38b3c9fc87a7e6796036b13b391248d5
Yüsra <76865219+devyusss@users.noreply.github.com>
2022-02-13 14:05:01 +0300

    Update ProductService.java

--- Day7_northwind/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ Day7_northwind/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -10,21 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProductService {
     DataResult<List<Product>> getAll();
-    DataResult<List<Product>> getAllSorted(); //datayı ıstenen sarta gore sıralar
-    DataResult<List<Product>> getAll(int pageNo, int pageSize);  //sayfalama, bir ssayfada belli dataların olması
     Result add(Product product);
-    
-    DataResult<Product> getByProductName(String productName);
-	
-    DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
-
-    DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
-
-    DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
-
-    DataResult<List<Product>> getByProductNameContains(String productName);
-
-    DataResult<List<Product>> getByProductNameStartsWith(String productName);
-
-    DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
+ 
 }
