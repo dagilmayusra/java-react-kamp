2af5772a0f12f491e73d46ae4976e6b50948e82c
Yüsra <76865219+devyusss@users.noreply.github.com>
2022-02-13 14:07:16 +0300

    Update ProductsController.java

--- Day7_northwind/northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ Day7_northwind/northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -9,51 +9,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController		//controller oldugunu soyluyor
-@RequestMapping("/api/products")		//adres uzerinden ıstek gelırse bu karsılar
+@RestController		
+@RequestMapping("/api/products")		
 public class ProductsController {
 	
-	//birden fazla service kullanıldıgı zamn autowired kullanılması gerek
-	private final ProductService productService;
+    private final ProductService productService;
 
     @Autowired
     public ProductsController(ProductService productService){
         this.productService = productService;
     }
 
-	@GetMapping("/getall")	//istekte bulunduğu yeri çalıştırır
-	 public DataResult<List<Product>> getAll(){
+    @GetMapping("/getall")	
+    public DataResult<List<Product>> getAll(){
         return productService.getAll();
     }
 
     @PostMapping("/add")
     public Result add(@RequestBody Product product){
         return productService.add(product);
-    }
-    
-    @GetMapping("/getByProductName")
-    public DataResult<Product> getByProductName(@RequestParam String productName){
-    	//requestparam yapılan istegin parametrelerine bak orda productname oku 
-    	return this.productService.getByProductName(productName);
-    }
-	
-    @GetMapping("/getByProductNameAndCategoryId")
-    public DataResult<Product> getByProductNameAndCategoryId(@RequestParam String productName, @RequestParam int categoryId){
-    	return this.productService.getByProductNameAndCategoryId(productName, categoryId);
-    }
-    
-    @GetMapping("/getByProductNameContains")
-    public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){
-    	return this.productService.getByProductNameContains(productName);
-    }
-    
-    @GetMapping("/getAllByPage")
-    DataResult<List<Product>> getAll(int pageNo, int pageSize){
-    	return this.productService.getAll(pageNo, pageSize);
-    }
-    
-    @GetMapping("getAllDesc")
-    public DataResult<List<Product>> getAllSorted(){
-    	return this.productService.getAllSorted(); 
     }
 } 
